f9600b5d2248dabe7e6a3800ec51fd9a510761c9
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-05-31 11:53:53 +0900

    Refactoring

--- collage_manual.py
+++ collage_manual.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import numpy as np
 from moviepy.editor import VideoFileClip, clips_array, concatenate_videoclips
 
@@ -10,10 +8,12 @@ videos = (
     "/nas.dbms/randy/datasets/ucf101-mix-scene-video-inpaint/Basketball/v_Basketball_g08_c04.mp4",
 )
 
-output_path = Path("/nas.dbms/randy/datasets/Collage 3.mp4")
-clip = [VideoFileClip(str(video)) for video in videos]
-clip = np.array(clip).reshape(2, 2)
-clip = clips_array(clip)
+collage_shape = 2, 2
+output_path = "/nas.dbms/randy/datasets/Collage 3.mp4"
 
-final_video = concatenate_videoclips([clip])
-final_video.without_audio().write_videofile(str(output_path))
+clips = [VideoFileClip(v) for v in videos]
+collage = np.array(clips).reshape(collage_shape)
+collage = clips_array(clips)
+
+final_video = concatenate_videoclips([collage])
+final_video.without_audio().write_videofile(output_path)
