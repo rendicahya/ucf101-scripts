f9293b43c62b0f11ac49864685659f00392f968e
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-22 16:24:47 +0900

    Separated XGTF parser to different file

---
+++ xgtf_parser.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from typing import Union
+
+from assertpy.assertpy import assert_that
+from bs4 import BeautifulSoup
+
+
+def parse(path: Union[Path, str], action_only: bool = False):
+    assert_that(path).is_file().is_readable()
+
+    with open(path) as f:
+        try:
+            soup = BeautifulSoup(f, "xml")
+        except:
+            print("Error reading:", path)
+            return None
+
+    all_bbox = {}
+
+    for sourcefile in soup.find("data").find_all("sourcefile"):
+        for person in sourcefile.find_all("object", {"name": "PERSON"}):
+            person_id = int(person["id"])
+
+            if person_id in all_bbox:
+                continue
+
+            person_locations = person.find("attribute", {"name": "Location"})
+            person_bbox = {}
+            person_action = person.find("data:bvalue", {"value": "true"})
+
+            if not person_action:
+                continue
+
+            act_start, act_end = [int(i) for i in person_action["framespan"].split(":")]
+
+            for bbox in person_locations.find_all("data:bbox"):
+                start, end = [int(i) for i in bbox["framespan"].split(":")]
+
+                if action_only:
+                    if act_start <= start <= act_end or act_start <= end <= act_end:
+                        start = max(start, act_start)
+                        end = min(end, act_end)
+
+                        for frame in range(start - 1, end):
+                            bbox_data = {
+                                frame: (
+                                    int(bbox["x"]),
+                                    int(bbox["y"]),
+                                    int(bbox["width"]),
+                                    int(bbox["height"]),
+                                )
+                            }
+
+                            person_bbox.update(bbox_data)
+                else:
+                    for frame in range(start - 1, end):
+                        bbox_data = {
+                            frame: (
+                                int(bbox["x"]),
+                                int(bbox["y"]),
+                                int(bbox["width"]),
+                                int(bbox["height"]),
+                            )
+                        }
+
+                        person_bbox.update(bbox_data)
+
+            all_bbox.update({person_id: person_bbox})
+
+    return all_bbox
