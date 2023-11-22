37f7841668a9a26e33ca409a7ac723354a5217a8
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-23 07:35:12 +0900

    Fixed multithreading

--- xgtf_to_video_bbox.py
+++ xgtf_to_video_bbox.py
@@ -1,19 +1,16 @@
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from pathlib import Path
+from typing import Callable
 
 import cv2
-import utils
-from bs4 import BeautifulSoup
-from moviepy.editor import ImageSequenceClip, VideoFileClip
+import xgtf_parser
+from assertpy.assertpy import assert_that
 from python_config import Config
-
-conf = Config("config.json")
-annotation_path = Path(conf.xgtf.path)
-input_path = Path(conf.ucf101.path)
-output_path = Path(conf.output.video_bbox)
-bbox_thickness = 2
-
-print_frame_number = False
-action_only = False
+from python_file import count_files
+from python_video import frames_to_video, video_frames, video_info
+from tqdm import tqdm
 
 colors = (
     (0, 0, 255),
@@ -26,84 +23,17 @@ colors = (
 )
 
 
-def operation(action, anno_file):
-    if anno_file.suffix != ".xgtf":
+def draw_bbox(xgtf, video_path, conf):
+    people_bbox = xgtf_parser.parse(xgtf)
+
+    if people_bbox is None:
         return
 
-    with open(anno_file) as f:
-        try:
-            soup = BeautifulSoup(f, features="xml")
-        except:
-            return
-
-    data = soup.find("data")
-    people_bbox = {}
-
-    if data is None:
-        return
-
-    for sourcefile in data.find_all("sourcefile"):
-        for person in sourcefile.find_all("object", {"name": "PERSON"}):
-            person_id = int(person["id"])
-
-            if person_id in people_bbox:
-                continue
-
-            person_locations = person.find("attribute", {"name": "Location"})
-            person_bbox = {}
-            person_action = person.find("data:bvalue", {"value": "true"})
-
-            if not person_action:
-                continue
-
-            act_start, act_end = [int(i) for i in person_action["framespan"].split(":")]
-
-            for bbox in person_locations.find_all("data:bbox"):
-                start, end = [int(i) for i in bbox["framespan"].split(":")]
-
-                if action_only:
-                    if act_start <= start <= act_end or act_start <= end <= act_end:
-                        start = max(start, act_start)
-                        end = min(end, act_end)
-
-                        for frame in range(start - 1, end):
-                            bbox_data = {
-                                frame: (
-                                    int(bbox["x"]),
-                                    int(bbox["y"]),
-                                    int(bbox["width"]),
-                                    int(bbox["height"]),
-                                )
-                            }
-
-                            person_bbox.update(bbox_data)
-                else:
-                    for frame in range(start - 1, end):
-                        bbox_data = {
-                            frame: (
-                                int(bbox["x"]),
-                                int(bbox["y"]),
-                                int(bbox["width"]),
-                                int(bbox["height"]),
-                            )
-                        }
-
-                        person_bbox.update(bbox_data)
-
-            people_bbox.update({person_id: person_bbox})
-
-    input_video_path = input_path / action.name / (anno_file.with_suffix(".avi").name)
-    output_video_path = output_path / action.name / (anno_file.with_suffix(".mp4").name)
-
-    output_video_path.parent.mkdir(parents=True, exist_ok=True)
-
-    clip = VideoFileClip(str(input_video_path))
-    frames = clip.iter_frames()
-    output_frames = []
+    frames = video_frames(video_path)
 
     for i, frame in enumerate(frames):
         for person_id, person_bbox in people_bbox.items():
-            if print_frame_number:
+            if conf.video_bbox.output.print_frame_number:
                 cv2.putText(
                     frame,
                     str(i + 1),
@@ -127,15 +57,86 @@ def operation(action, anno_file):
                 (x1, y1),
                 (x2, y2),
                 colors[person_id % len(colors)],
-                bbox_thickness,
+                conf.video_bbox.output.bbox_thickness,
             )
 
-        output_frames.append(frame)
+        yield frame
 
-    if len(output_frames) > 0:
-        clip = ImageSequenceClip(output_frames, fps=clip.fps)
-        clip.write_videofile(str(output_video_path), audio=False)
+
+def process_video_frame(frames: list, operation: Callable):
+    for frame in frames:
+        yield operation(frame)
+
+
+def core_job(xgtf, input_video_path, output_video_path, conf, bar):
+    bbox_frames = draw_bbox(xgtf, input_video_path, conf)
+    fps = video_info(input_video_path)["fps"]
+    grayscale_op = lambda f: cv2.cvtColor(f, cv2.COLOR_BGR2RGB)
+    bbox_frames_rgb = process_video_frame(bbox_frames, grayscale_op)
+
+    bar.set_description(xgtf.stem)
+    output_video_path.parent.mkdir(parents=True, exist_ok=True)
+
+    frames_to_video(
+        bbox_frames_rgb,
+        target=output_video_path,
+        writer=conf.video_bbox.video_writer,
+        fps=fps,
+    )
+
+    bar.update(1)
+
+
+def main():
+    conf = Config("config.json")
+    xgtf_path = Path(conf.xgtf.path)
+    input_path = Path(conf.ucf101.path)
+    output_path = Path(conf.video_bbox.output.path)
+
+    assert_that(xgtf_path).is_directory().is_readable()
+    assert_that(input_path).is_directory().is_readable()
+
+    n_files = count_files(xgtf_path)
+    bar = tqdm(total=n_files)
+    n_cores = multiprocessing.cpu_count()
+
+    if conf.video_bbox.multithread:
+        print(f"Running jobs on {n_cores} cores...")
+
+    with ThreadPoolExecutor(max_workers=n_cores) as executor:
+        futures = []
+
+        for action in xgtf_path.iterdir():
+            for xgtf in action.iterdir():
+                if xgtf.suffix != ".xgtf":
+                    continue
+
+                input_video_path = (
+                    input_path / action.name / (xgtf.with_suffix(conf.ucf101.ext).name)
+                )
+
+                output_video_path = (
+                    output_path / action.name / (xgtf.with_suffix(".mp4").name)
+                )
+
+                if conf.video_bbox.multithread:
+                    futures.append(
+                        executor.submit(
+                            partial(
+                                core_job,
+                                xgtf,
+                                input_video_path,
+                                output_video_path,
+                                conf,
+                                bar,
+                            )
+                        )
+                    )
+                else:
+                    core_job(xgtf, input_video_path, output_video_path, conf, bar)
+
+    bar.close()
 
 
 if __name__ == "__main__":
-    utils.iterate(annotation_path, operation, extension=".xgtf")
+    main()
